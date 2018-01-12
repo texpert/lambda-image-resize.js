486eaadd19e0847dfb16daaccfefbf680cc65055
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2018-01-12 17:02:10 +0200

    Original file is copied to the `original.targetStorage` bucket.
    `to_format` is run conditionally, only if a format is specified in `version` hash.
    
    The resulting objects includes `original` key and keys with the versions' names.

--- lambda/index.js
+++ lambda/index.js
@@ -11,22 +11,25 @@ exports.handler = function(event, context, callback) {
   console.log('Versions:', versions);
   const originalBucket = findBucket(storages, original.storage);
   const originalKey = originalBucket.prefix ? `${originalBucket.prefix}/${original.id}` : original.id;
-
-  callback(null, { statusCode: '200', body: { 'Message': 'Lambda started' } });
-
+  const originalTargetBucket = findBucket(storages, original.targetStorage);
 
   S3.getObject({ Bucket: originalBucket.name, Key: originalKey }).promise()
+    .then(
+      callback(null, { statusCode: '200', body: { 'Message': 'Lambda started' } })
+    )
     .then(function (data) {
       const versionPromises = [];
       for(const version of versions) {
-        versionPromises.push(resizeToBucket(data, storages, path, version)
+        versionPromises.push(resizeToBucket(data, findBucket(storages, version.storage), path, version)
           .catch(reason => console.log(`Error on version: ${version.name}, reason: ${reason}`))
         )
       }
+      versionPromises.push(bufferToBucket(data.Body, originalTargetBucket.name,
+        formPath(originalTargetBucket, path, original), 'original'));
       Promise.all(versionPromises)
-             .then(function(values) {
-               console.log('values:', values);
-             })
+        .then(function(values) {
+          console.log('values:', values);
+        })
     })
     .catch(err => callback(err));
 };
@@ -35,26 +38,41 @@ function findBucket(storages, storageName) {
   return (storages.find(function (obj) { return storageName in obj }))[storageName];
 }
 
-function resizeToBucket(data, storages, path, version) {
+function resizeToBucket(data, bucket, path, version) {
   return new Promise(function(resolve) {
-    const versionBucket = findBucket(storages, version.storage);
-    Sharp(data.Body)
-      .resize(version.width, version.height)
-      .toFormat(version.format)
-      .toBuffer()
-      .then((buffer) => {
-        let versionKey = versionBucket.prefix ? `${versionBucket.prefix}/${path}` : path;
-        versionKey = versionKey.split('/');
-        const lastElementIndex = versionKey.length - 1;
-        versionKey[lastElementIndex] = `${version.name}-${versionKey[lastElementIndex]}.${version.format}`;
-        versionKey = versionKey.join('/');
-        S3.putObject({ Body : buffer,
-                       Bucket : versionBucket.name,
-                       Key : versionKey },
-          () => { version.key = versionKey;
-                   console.log('Image processed:', version.name);
-                   resolve(version); }
-        );
-      })
+    const pipeline = Sharp(data.Body).resize(version.width, version.height);
+    const format = version.format;
+    if (format)
+      pipeline.toFormat(format);
+    pipeline.toBuffer()
+    .then((buffer) => {
+      resolve(bufferToBucket(buffer, bucket.name, formPath(bucket, path, version), version.name));
+    })
   });
+}
+
+function bufferToBucket(buffer, bucket, id, name) {
+  return new Promise(function(resolve) {
+    S3.putObject({
+        Body : buffer,
+        Bucket : bucket,
+        Key : id },
+      () => { console.log('Buffer stored:', id);
+              resolve({ [`${name}`]: { store : bucket, id : id } }) }
+    );
+  })
+}
+
+function formPath({ prefix = null }, path, { format = null, id = null, name = null }) {
+  let result = prefix ? `${prefix}/${path}` : path;
+  if (name) {
+    result = result.split('/');
+    const lastElementIndex = result.length - 1;
+    let filename = `${name}-${result[lastElementIndex]}`;
+    if (format)
+      filename = `${filename}.${format}`;
+    result[lastElementIndex] = filename;
+    result = result.join('/');
+  }
+  return result;
 }
